5081237279e6d78ec03497db92aa8567a19677a0
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-28 23:08:26 +1300

    Added styling in App.css

--- src/App.js
+++ src/App.js
@@ -14,31 +14,31 @@ function App() {
   // use the products variable to read all of your products
   // and display them on your page
   const [products, setProducts] = useState([]);
-  const [filterBy, setFilterBy] = useState("all");
-  const [sortBy, setSortBy] = useState("latest");
-  const [filteredProducts, setFilteredProducts] = useState(getProducts())
+  // const [filterBy, setFilterBy] = useState("all");
+  // const [sortBy, setSortBy] = useState("latest");
+  // const [filteredProducts, setFilteredProducts] = useState(getProducts())
 
   useEffect(() => {
     const loadData = async () => {
       const products = await getProducts();
-      // setProducts(products);
+      setProducts(products);
     
-    const filtered = filterByCategory(products,filterBy);
-    const sorted = sortProducts(filtered, sortBy);
-    setFilteredProducts([...sorted]);
+    // const filtered = filterByCategory(products,filterBy);
+    // const sorted = sortProducts(filtered, sortBy);
+    // setFilteredProducts([...sorted]);
     };
 
     loadData();
-  }, [filterBy, sortBy, products]);
+  }, []);
 
   return (
     <div className="container">
-      <h1>Online Shop</h1>
+      <h1>Pampurred Cats</h1>
       <div className="toolbar">
-       <FilterBy setFilteBy={setFilterBy} />
-       <SortBy setSortBy={setSortBy}/>
+       <FilterBy  />
+       <SortBy />
       </div>
-       <Products products={filteredProducts} />
+       <Products products={products} />
     
     </div>
     
