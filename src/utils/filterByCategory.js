83342ceaef29ae35a5657a8a644575afdfa97e85
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-03-29 15:53:32 +1300

    Added const result to filter metadata

--- src/utils/filterByCategory.js
+++ src/utils/filterByCategory.js
@@ -2,8 +2,9 @@ const filterByCategory = (products, filterBy) => {
     if (filterBy === "all") {
       return products;
     }
+  const result = [...products.filter((product) => product.metadata.category ===filterBy)];
   
-    return products.filter((product) => product.metadata.category ===filterBy);
+  return result;
 };
 
 export default filterByCategory;
